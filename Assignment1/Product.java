84fa53920f4ebbe8a9185d1ff5fded5e3f2dca62
Abdullah-1922 <kafikafi1922@gmail.com>
2025-09-24 22:47:47 +0600

    assignment 1 basic print in java

---
+++ Assignment1/Product.java
@@ -0,0 +1,14 @@
+// Assignment 1
+package Assignment1;
+
+public class Product {
+
+    public static void main(String[] args) {
+        System.out.print("id: 101,\n");
+        System.out.print("title: iphone15,\n");
+        System.out.print("price: 1895 euros,\n");
+        System.out.print("description: perfect product with best image quality,\n");
+        System.out.print("category: phone");
+    }
+
+}
