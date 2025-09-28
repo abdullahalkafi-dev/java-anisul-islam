b93aabcde36d1eed549a25b578bd43e570707025
Abdullah-1922 <kafikafi1922@gmail.com>
2025-09-28 14:05:03 +0600

    basic of comment in java

---
+++ Basic_Java/Java_comments.java
@@ -0,0 +1,28 @@
+package Basic_Java;
+
+public class Java_comments {
+    public static void main(String[] args) {
+        // this ( // ) used for single line comment
+        /*
+          this
+          is
+          for
+          multiline
+          comment 
+         */
+
+        /*
+        Why comment is important ? 
+        1. comments help to explain what the code is doing in my program.
+        2. It helps the person reading the code more easily.
+        */ 
+
+       // This code calculates and prints the sum of two numbers
+       int a= 5;    
+       int b= 6;
+       int sum = a+b;
+       System.out.println(sum);
+
+
+    }
+}
