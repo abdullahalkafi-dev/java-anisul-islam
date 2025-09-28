4bcd34b86f123ef2265c9c8e31765e37ebeaeca2
Abdullah-1922 <kafikafi1922@gmail.com>
2025-09-28 13:13:51 +0600

    basic escape sequence

--- Assignment1/Product.java
+++ Assignment1/Product.java
@@ -4,11 +4,11 @@ package Assignment1;
 public class Product {
 
     public static void main(String[] args) {
-        System.out.print("id: 101,\n");
-        System.out.print("title: iphone15,\n");
-        System.out.print("price: 1895 euros,\n");
-        System.out.print("description: perfect product with best image quality,\n");
-        System.out.print("category: phone");
+        System.out.println("id: 101,");
+        System.out.println("title: iphone15,");
+        System.out.println("price: 1895 euros,");
+        System.out.println("description: perfect product with best image quality,");
+        System.out.println("category: phone");
     }
 
 }
---
+++ Basic_Java/Java_escape_sequence.java
@@ -0,0 +1,10 @@
+package Basic_Java;
+
+public class Java_escape_sequence {
+    public static void main (String[]args){
+     System.out.println("Print with new line");
+     System.out.print("Print with new line\n");  // with  newline escape sequence
+     System.out.print("hello \t world\n");  // print with tab escape sequence
+     System.out.print("\"Abdullah Al kafi\""); 
+    }
+}
